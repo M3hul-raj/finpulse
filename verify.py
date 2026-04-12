59d39fde810eae0c55b8847931524c00c4ca1fe6
Mehul raj <your_registered_email@gmail.com>
2026-04-12 11:52:27 +0530

    chore: remove verify.py debug script
    Signed-off-by: Mehul raj <your_registered_email@gmail.com>

--- verify.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv('data/historical.csv')
-stats = df.groupby('segment')['balance'].agg(['min', 'mean', 'max']).round(0)
-print(stats.to_string())
-print("\nAny negative balances:", (df['balance'] < 0).sum())
